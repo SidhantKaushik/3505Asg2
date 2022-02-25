34cc1a1660f1c0f3a1433b00c1b0e218f254e3da
Sidhant Kaushik <59937333+SidhantKaushik@users.noreply.github.com>
2022-02-25 15:24:14 -0700

    Update DataUtilitiesTest.java

--- test/org/jfree/data/DataUtilitiesTest.java
+++ test/org/jfree/data/DataUtilitiesTest.java
@@ -144,43 +144,6 @@ class DataUtilitiesTest {
 		assertThrows(InvalidParameterException.class, () -> DataUtilities.calculateRowTotal(invalidValue, 0));
 	}
 	
-	//test number array 2D
-	@Test 
-	void createNumberArray2D() {
-		double[][] data = new double [2][];
-		data[0] = new double[] {1.0, 2.0, 2.5}; 
-		data[1] = new double[] {1.1, 2.1, 2.6, 3.3}; 
-
-		//verify
-		Number[][] actual = DataUtilities.createNumberArray2D(data);
-		assertEquals(2, actual.length);
-		assertEquals(3, actual[0].length);
-		assertEquals(4, actual[1].length);
-
-	}
-
-	@Test 
-	void createNumberArray2D2() {
-		double[][] data = new double [3][];
-		data[0] = new double[] {-1.0, -2.5}; 
-		data[1] = new double[] {-1.1, -2.1, -2.6}; 
-		data[2] = new double[] {-1.4, -2.9, -4.2}; 
-
-		//verify
-		Number[][] actual = DataUtilities.createNumberArray2D(data);
-		assertEquals(3, actual.length);
-		assertEquals(2, actual[0].length);
-		assertEquals(3, actual[1].length);
-		assertEquals(3, actual[2].length);
-
-	}
-
-	//test the NULL method for createNumberArray2D
-	@Test
-	void createNumberArray2DNull() {
-		assertThrows(InvalidParameterException.class, () -> DataUtilities.createNumberArray2D(null));
-	}
-
 
 	//test cumulative percentages 
 	@Test
